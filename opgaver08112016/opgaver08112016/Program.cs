19b21e3a1f145803166c91fb1b22f58dfdde7c00
allanstorgaard <allanstorgaard@outlook.dk>
2016-11-08 14:24:39 +0100

    opdatering klokken 14:24

--- opgaver08112016/opgaver08112016/Program.cs
+++ opgaver08112016/opgaver08112016/Program.cs
@@ -10,9 +10,15 @@ namespace opgaver08112016
     {
         static void Main(string[] args)
         {
+
             //opgave11();
-            opgave12();
+            //opgave12();
+            //opgave13();
+            //opgave14();
+            //opgave15();
+            opgave16();
         }
+
         public static void opgave11()
         {
             Console.WriteLine("Hello\nAllan");
@@ -24,5 +30,49 @@ namespace opgaver08112016
             int sum = x + y;
             Console.WriteLine("{0} + {1} = {2}", x, y, sum);
         }
+
+        public static void opgave13()
+        {
+            float x = 24, y = 5, result = x / y;
+            Console.WriteLine("{0}/{1} = {2}", x, y, result);
+        }
+        
+        public static void opgave14()
+        {
+            Console.WriteLine("Result: {0}", -1+3*5f);
+            Console.WriteLine("Result: {0}", (24+5)%7f);
+            Console.WriteLine("Result: {0}", 15f+((-4f*6f)/11f));
+            Console.WriteLine("Result: {0}", 2+10/6*1-7%2f);
+        }
+
+        public static void opgave15()
+        {
+            Console.WriteLine("Indtast første heltal");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Indtast andet heltal");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Summen er: {0}", x+y);
+        }
+
+        public static void opgave16()
+        {
+            Console.WriteLine("***Allan Storgaard***");
+            Console.WriteLine("Indtast første heltal");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Indtast andet heltal");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Indtast tredie heltal");
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("Summen er: {0}", x * y * z);
+        }
+
+        public static void opgave17()
+        {
+
+        }
+
+
+
+
     }
 }
